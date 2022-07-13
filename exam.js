d135f5cd0bf5b13c6a60adf22be58ea9cbce7734
OnifadeLois <aoonifade@gmail.com>
2022-07-14 02:43:07 +0900

    Exam-1

---
+++ exam.js
@@ -0,0 +1,10 @@
+function sumOfNumbers(arrayOfNumbers) {
+    return arrayOfNumbers.length;
+        const numbers = [68, -68, 27, 94, 72, -25, -51, 32, 10, 64, -94, 4, 34, -86, 90, 81, 20, -56, -91, -50];
+let     sum = 0;
+
+for(let i =0; i< numbers.length; i++) 
+    sum += numbers[i];
+
+console.log(sum);
+}
